675d8c1bc7eec6caf16c3b089dab1d1245ffc30a
Nebularaid2000 <987082268@qq.com>
2020-03-09 21:26:35 +0800

    开始界面和关卡选择UI

---
+++ Assets/script/UI/Select.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Select : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayGame1()
+    {
+        SceneManager.LoadScene("Scratch");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
