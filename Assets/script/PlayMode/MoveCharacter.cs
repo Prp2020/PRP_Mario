cc9a7c24d463a6bb0863625cdc840047d091fa8f
Tiancheng Yang <58246222+werewolf-ytc@users.noreply.github.com>
2020-03-10 15:38:50 +0800

    玩家用方向键玩的游戏界面完成
    在PlayMode这一scene中，使用方向键可以游玩。

--- Assets/script/PlayMode/MoveCharacter.cs
+++ Assets/script/PlayMode/MoveCharacter.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class MoveCharacter : Mario
 {
     private float Time_ = 1f;   //"1f" is just to make sure Time_ is initially big enough
-    private readonly float Speed = 0.05f;
+    private readonly float Speed = 0.025f;
     private int dir;//dir = 0 -> move nowhere. 1-left;2-up;3-right;4-down.
     private Vector3 Origin;
+    private PlayMode_Data TheData;
+    private bool ResetGold;
     // Start is called before the first frame update
     public override void Start()
     {
+        ResetGold = true;
         Origin = this.transform.position;
         dir = 0;
+        TheData = (PlayMode_Data)GameObject.Find("pool").GetComponent("PlayMode_Data");
     }
 
     // Update is called once per frame
@@ -44,30 +48,44 @@ public class MoveCharacter : Mario
 
         if (Time_ > ResetTime_)
         {
-            this.transform.position = Origin;
+            this.transform.position = Origin;//重置角色位置
+            if (ResetGold)
+            {
+                TheData.Reset();//重置金币数量
+                ResetGold = false;
+            }
+            //移动角色
             if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 Time_ = 0;
                 dir = 3;
                 this.transform.position -= Vector3.left * Speed;
+                TheData.MoveTo(3);
+                ResetGold = true;
             }
             if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
                 Time_ = 0;
                 dir = 1;
                 this.transform.position += Vector3.left * Speed;
+                TheData.MoveTo(1);
+                ResetGold = true;
             }
             if (Input.GetKeyDown(KeyCode.UpArrow))
             {
                 Time_ = 0;
                 dir = 2;
                 this.transform.position += Vector3.up * Speed;
+                TheData.MoveTo(2);
+                ResetGold = true;
             }
             if (Input.GetKeyDown(KeyCode.DownArrow))
             {
                 Time_ = 0;
                 dir = 4;
                 this.transform.position -= Vector3.up * Speed;
+                TheData.MoveTo(4);
+                ResetGold = true;
             }
         }
     }
